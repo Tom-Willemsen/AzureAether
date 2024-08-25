d45fb5f154134be1b478a46221cb94adafbe2b61
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-08-25 10:47:05 +0100

    Move DAE normalization to trigger()
    It might take a while, potentially.

--- src/azureaether/simpledae.py
+++ src/azureaether/simpledae.py
@@ -1,8 +1,5 @@
 import asyncio
-from typing import Dict
 
-from bluesky.protocols import Reading
-from event_model import DataKey
 from ibex_bluesky_core.devices.dae import Dae
 from ophyd_async.core import AsyncStatus
 
@@ -13,15 +10,3 @@ class SimpleDae(Dae):
         await self.begin_run.trigger(wait=True, timeout=None)
         await asyncio.sleep(3)
         await self.end_run.trigger(wait=True, timeout=None)
-
-    async def read(self) -> dict[str, Reading]:
-        good_uah_reading = await self.good_uah.read()
-        return {
-            self.name: {
-                "value": good_uah_reading[self.good_uah.name]["value"],
-                "timestamp": good_uah_reading[self.good_uah.name]["timestamp"],
-            }
-        }
-
-    async def describe(self) -> Dict[str, DataKey]:
-        return {self.name: {"dtype": "number", "shape": [], "source": self.good_uah.source}}
--- src/azureaether/uncertainty.py
+++ src/azureaether/uncertainty.py
@@ -1,3 +1,4 @@
+import asyncio
 import math
 from typing import TypeVar
 
@@ -7,73 +8,80 @@ import scipp as sc
 from bluesky.callbacks import LiveTable
 from ibex_bluesky_core.devices import get_pv_prefix
 from ibex_bluesky_core.devices.block import block_rw
-from ophyd_async.core import DeviceVector, HintedSignal, StandardReadable
+from ophyd_async.core import (
+    AsyncStatus,
+    DeviceVector,
+    HintedSignal,
+    StandardReadable,
+    soft_signal_rw,
+)
 from ophyd_async.epics.signal import epics_signal_r
 from ophyd_async.plan_stubs import ensure_connected
 
-from azureaether.haven_derived_signal import derived_signal_r
 from azureaether.simpledae import SimpleDae
 
 T = TypeVar("T")
 
 
 def plan():
-    dae = SimpleDae(get_pv_prefix())
     mot = block_rw(float, "mot")
 
-    class DaeWithUncertainty(StandardReadable):
-        def __init__(self, name):
+    class Spectrum(StandardReadable):
+        def __init__(self, spec_num: int, name=""):
+            self.y = epics_signal_r(
+                np.typing.NDArray[np.float32], f"TE:NDW2922:DAE:SPEC:1:{spec_num}:Y"
+            )
+            self.x = epics_signal_r(
+                np.typing.NDArray[np.float32], f"TE:NDW2922:DAE:SPEC:1:{spec_num}:X"
+            )
+            super().__init__(name=name)
+
+    class DaeWithUncertainty(SimpleDae):
+        def __init__(self, prefix: str, name: str):
             self.nspec = 250
-            self.spec = DeviceVector(
-                {
-                    i: epics_signal_r(np.typing.NDArray[np.float32], f"TE:NDW2922:DAE:SPEC:1:{i}:Y")
-                    for i in range(1, self.nspec + 1)
-                }
+            self.spec = DeviceVector({i: Spectrum(spec_num=i) for i in range(1, self.nspec + 1)})
+
+            with self.add_children_as_readables(HintedSignal):
+                self.val = soft_signal_rw(float, 0.0)
+            with self.add_children_as_readables():
+                self.err = soft_signal_rw(float, 0.0)
+
+            super().__init__(prefix, name)
+
+        @AsyncStatus.wrap
+        async def trigger(self) -> None:
+            await super().trigger()
+            await self._normalize_trigger()
+
+        async def _normalize_trigger(self) -> None:
+            # Read all spectra
+            spectra = await asyncio.gather(*(spec.y.get_value() for spec in self.spec.values()))
+
+            # Run normalization in a separate thread so that we don't block the main event loop
+            # if it takes a while.
+            val, err = await asyncio.to_thread(self._normalize, spectra)
+
+            await asyncio.gather(
+                self.val.set(val, wait=True, timeout=None),
+                self.err.set(err, wait=True, timeout=None),
             )
 
-            with self.add_children_as_readables(HintedSignal.uncached):
-                self.norm = derived_signal_r(
-                    np.typing.NDArray[np.float64],
-                    [0.0, 0.0],
-                    derived_from={f"spec_{i}": self.spec[i] for i in range(1, self.nspec + 1)},
-                    inverse=self._normalize,
-                    units="",
-                    precision=5,
-                    monitor_and_cache=False
-                )
-                self.val = derived_signal_r(
-                    float,
-                    initial_value=0.0,
-                    derived_from={"norm": self.norm},
-                    inverse=lambda values, *, norm: values[norm][0],
-                    monitor_and_cache=False
-                )
-                self.err = derived_signal_r(
-                    float,
-                    initial_value=0.0,
-                    derived_from={"norm": self.norm},
-                    inverse=lambda values, *, norm: values[norm][1],
-                    monitor_and_cache=False
-                )
-            super().__init__(name)
-            self.norm.set_name(name)
-
-        def _normalize(self, values, **kwds):
+        def _normalize(self, values):
             data = sc.concat(
-                [sc.array(dims=["tof"], values=v, variances=v) for v in values.values()],
+                [sc.array(dims=["tof"], values=v, variances=v) for v in values],
                 dim="spectrum",
             )
 
             monitors = data["spectrum", 0:10]
-            detectors = data["spectrum", 10:self.nspec]
+            detectors = data["spectrum", 10 : self.nspec + 1]
 
             result = sc.sum(detectors) / sc.sum(monitors)
-            return [float(result.value), float(math.sqrt(result.variance))]
+            return float(result.value), float(math.sqrt(result.variance))
 
-    dae_err = DaeWithUncertainty("DaeWithUncertainty")
+    dae = DaeWithUncertainty(get_pv_prefix(), "DAE")
 
-    yield from ensure_connected(dae, mot, dae_err)
-    yield from bp.scan([dae, dae_err], mot, 1, 3, 5)
+    yield from ensure_connected(dae, mot)
+    yield from bp.scan([dae], mot, 1, 3, 5)
 
 
 if __name__ == "__main__":
@@ -86,9 +94,8 @@ if __name__ == "__main__":
             LiveTable(
                 [
                     "mot",
-                    "DAE",
-                    "DaeWithUncertainty-val",
-                    "DaeWithUncertainty-err",
+                    "DAE-val",
+                    "DAE-err",
                 ]
             ),
         ],
